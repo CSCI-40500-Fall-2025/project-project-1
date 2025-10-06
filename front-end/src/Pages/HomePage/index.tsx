55399788d1eb610e2146f7aa94d3a8dc354c1651
Ying <yjiang0229@gmail.com>
2025-10-06 15:30:33 -0400

    clean up imports

--- front-end/src/Pages/HomePage/index.tsx
+++ front-end/src/Pages/HomePage/index.tsx
@@ -1,6 +1,4 @@
 import Box from "@mui/material/Box";
-// import React from "react";
-//import { getUser, logoutUser, checkLogin } from "../../services/userServices";
 import { useAuth } from "../../AuthContext";
 
 const HomePage = () => {
